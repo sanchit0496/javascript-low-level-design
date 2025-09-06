dd077dcd5955f12481c56feed93eb119e85a4fe0
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2025-09-07 01:17:43 +0530

    adding checkout service for payment

--- oops_concept/interface.ts
+++ oops_concept/interface.ts
@@ -14,11 +14,40 @@ class creditCard implements Payment{
     }
 }
 
+
+// creating a checkout class
+class Checkout{
+    private paymentGateway: Payment
+
+    constructor(paymentGateway: Payment){
+        this.paymentGateway = paymentGateway
+    }
+     
+    setPaymentGateway(paymentGateway: Payment): void{
+        this.paymentGateway = paymentGateway
+    }
+
+    checkout(amount: number): void{
+    const result = this.paymentGateway.processPayment(amount)
+    console.log(result)    }
+
+}
+
 function main(): void{
     const upiPayment: Payment = new upi()
     const creditCardPayment: Payment = new creditCard() 
-    console.log(upiPayment.processPayment(2000))
-    console.log(creditCardPayment.processPayment(5000))
+
+    // console.log(upiPayment.processPayment(2000))
+    // console.log(creditCardPayment.processPayment(5000))
+
+    const checkoutService = new Checkout(upiPayment)
+    checkoutService.setPaymentGateway(upiPayment)
+    checkoutService.checkout(2000)
+
+    checkoutService.setPaymentGateway(creditCardPayment)
+    checkoutService.checkout(5000)
+
+
 }
 
 main()
